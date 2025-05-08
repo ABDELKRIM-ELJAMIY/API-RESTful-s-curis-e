36b9e48bd069219ad566e837ee798c440a46fe00
didi <eljamiyenaa@gmai.com>
2025-05-08 12:50:27 +0100

    Lancer le serveur Express et Connexion MongoDB

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Démarrer le serveur
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
